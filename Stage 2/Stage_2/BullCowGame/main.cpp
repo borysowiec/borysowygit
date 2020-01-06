dfe2b5c737fb9374588745c050b10b069515f360
borysowiec <borysowiec@gmail.com>
2020-01-06 14:27:23 +0100

    Completed 2.25

--- Stage 2/Stage_2/BullCowGame/main.cpp
+++ Stage 2/Stage_2/BullCowGame/main.cpp
@@ -3,23 +3,38 @@
 
 using namespace std;
 
+void PrintIntro();
+string GetGuessAndPrintItBack();
+
+//the entry point for the game
 int main()
 {
-	//introduce the game
+	PrintIntro();
+
+	GetGuessAndPrintItBack();
+	GetGuessAndPrintItBack();
+
+	cout << endl;
+	return 0;
+}
+
+//introduce the game
+void PrintIntro() {
 	constexpr int WORD_LENGHT = 5;
 	cout << "Welcome to Bulls and Cows" << endl;
 	cout << "Can you guess the " << WORD_LENGHT;
 	cout << " letter isogram I'm thinking of?\n";
 	cout << endl;
+	return;
+}
 
-	//get a guess from the player
+//get a guess from the player
+string GetGuessAndPrintItBack() {
 	cout << "Enter your guess: ";
 	string Guess = "";
-	cin >> Guess;
+	getline(cin, Guess);
 
-	//repeat the guess to the player
+	//print the guess to the player
 	cout << "Your guess was: " << Guess << endl;
-
-	cout << endl;
-	return 0;
+	return Guess;
 }
